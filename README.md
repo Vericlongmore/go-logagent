aec23f6f59d7c5f4dbebf9c242f3538aed26cea2
chenlong <chenlong@qutoutiao.net>
2018-02-03 18:35:52 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # go-logagent
 log collection written by go
+
+###go build  logagent/main
---
+++ src/logagent/main/server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"go_dev/day11/logagent/kafka"
+	"go_dev/day11/logagent/tailf"
+	"time"
+
+	"github.com/astaxie/beego/logs"
+	//"fmt"
+)
+
+func serverRun() (err error) {
+	for {
+		fmt.Println("ssss")
+		msg := tailf.GetOneLine()
+		fmt.Println(msg)
+		err = sendToKafka(msg)
+		if err != nil {
+			logs.Error("send to kafka failed, err:%v", err)
+			time.Sleep(time.Second)
+			continue
+		}
+	}
+	return
+}
+
+func sendToKafka(msg *tailf.TextMsg) (err error) {
+	fmt.Printf("read msg:%s, topic:%s\n", msg.Msg, msg.Topic)
+	err = kafka.SendToKafka(msg.Msg, msg.Topic)
+	return
+}
